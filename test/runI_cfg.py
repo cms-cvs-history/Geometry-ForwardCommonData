117416992965b5c16da256194d7c8d2359687f75
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2010-05-28 18:45:53 +0000

    Add geoemtry for HF light guide and fibre bundles

--- test/runI_cfg.py
+++ test/runI_cfg.py
@@ -3,6 +3,24 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("GEOM")
 process.load("Geometry.CMSCommonData.cmsAllGeometryXML_cfi")
 
+process.MessageLogger = cms.Service("MessageLogger",
+    destinations = cms.untracked.vstring('cout'),
+    categories = cms.untracked.vstring('HCalGeom'),
+    debugModules = cms.untracked.vstring('*'),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO'),
+        INFO = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        DEBUG = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        HCalGeom = cms.untracked.PSet(
+            limit = cms.untracked.int32(-1)
+        )
+    )
+)
+
 process.VisConfigurationService = cms.Service("VisConfigurationService",
     Views = cms.untracked.vstring('3D Window'),
     ContentProxies = cms.untracked.vstring('Simulation/Core', 
